8a761c7644e4dc6c99edb97c45dc37a4c4313910
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-02-21 17:54:48 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/* what happens when buy btn is pressed */
+$("#buybtn").on("click", function () {
+  alert("Nothing is real!");
+});
+
+/* what happens when subscribe btn is pressed */
+$("#subscribebtn").on("click", function () {
+  alert("You are not chosen");
+});
